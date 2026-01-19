eaa287b49e85be60335fc92f065b66c6359f2932
Kelvin Santos <kelvinsantosdealmeida@gmail.com>
2026-01-19 13:15:49 -0300

    consertando bug da variavel do sistema 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,22 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-
-
-load_dotenv()
+from dotenv import load_dotenv  # 1. Importe o load_dotenv
 
 db = SQLAlchemy()
-# migrate = Migrate()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    # Caminho para o banco dentro da pasta 'instance'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///../instance/sessions.db"
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    # 2. Carregue o config.env explicitamente
+    # Isso garante que as variáveis sejam lidas antes de usar o os.getenv abaixo
+    load_dotenv(os.path.join(os.getcwd(), 'config.env'))
+
+    # Agora o DATABASE_URL será encontrado
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///../instance/users.db")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
-    # # Inicializar extensões
     db.init_app(app)
-    # migrate.init_app(app, db)
 
     # Registrar blueprints
     from app.routes.session_routes import session_bp
